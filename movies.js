9c607fc1373b89489bf118f671cca6707b3667d8
AlFalahTaieb <taiebalfalah@gmail.com>
2020-04-27 18:08:30 +0200

    update movie

--- movies.js
+++ movies.js
@@ -1,5 +1,5 @@
 const MOVIES =
-["Stand by Me", "3-Iron", "The Sandlot", "Nobody Knows", "The Passenger", "Good Times", "Tampopo", "Never rarely sometimes always ", "Riding in cars with boys"]
+["Stand by Me", "3-Iron", "The Sandlot", "Nobody Knows", "The Passenger", "Good Times", "Tampopo", "Never rarely sometimes always ", "Riding in cars with boys","Vertigo","Straw Dogs"," On the Waterfront","The Apartment","Werckmeister harmóniák"]
 
 
 exports.movies = MOVIES
